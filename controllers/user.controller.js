184a17f103f71fa9d2b09a308cc73ca452d9e655
thamudi <tamim_hamoudi@outlook.com>
2024-02-10 16:36:30 +0300

    feat: add baseUrl to axios to imitate localhost

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -3,22 +3,21 @@
 require('dotenv').config()
 const axios = require("axios");
 
-
-const config = {
+const api = axios.create({
+  baseURL: process.env.BASE_URL || "http://localhost:3030",
   headers: {
     'X-Ig-App-Id': process.env.APP_ID,
     'Accept': '*/*',
     'User-Agent':'Instagram 126.0.0.25.121 Android (23/6.0.1; 320dpi; 720x1280; samsung; SM-A310F; a3xelte; samsungexynos7580; en_GB; 110937453)'
-  },
-};
-
+  }
+});
 
 let url = 'https://i.instagram.com/api/v1/users/web_profile_info/'
 
 exports.getUserProfileData = async (request, response) => {
   const username = request.params.name;
   
-  await axios.get(url+`?username=${username}`, config).then(res => {
+  await api.get(url+`?username=${username}`).then(res => {
     response.json(res.data)
   }).catch(error=>{
     console.error(error.message);
